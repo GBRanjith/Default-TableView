f5a1136d338e6b14c49f8194c606e2eb2268663e
Boopathy Ranjith <boopathyranjith0gmail.com>
2023-05-09 18:27:53 +0530

    new

--- Default TableView/ViewController.swift
+++ Default TableView/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    let datas = ["Ranjith","ravi","vinoth","arun"]
+      
+    @IBOutlet weak var tableview: UITableView!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableview.dataSource = self
+        tableview.delegate = self
+        
     }
-
-
+}
+extension ViewController: UITableViewDelegate,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = datas[indexPath.row]
+        return cell
+    }
 }
 
