b8015bdd67f4f82331fa2589a9dff0f4a3308854
Boopathy Ranjith <boopathyranjith0gmail.com>
2023-05-09 18:55:31 +0530

    updated

--- Default TableView/ViewController.swift
+++ Default TableView/ViewController.swift
@@ -12,13 +12,18 @@ class ViewController: UIViewController{
     let datas = ["Ranjith","ravi","vinoth","arun"]
       
     @IBOutlet weak var tableview: UITableView!
-    
-
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableview.dataSource = self
         tableview.delegate = self
+        setupui()
+        
+    }
+    
+    func setupui()
+    {
         
     }
 }
@@ -30,8 +35,24 @@ extension ViewController: UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = datas[indexPath.row]
         return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
+
+class viewcell: UITableViewCell
+{
+    @IBOutlet weak var bgview: UIView!
+    @IBOutlet weak var imgcontact: UIImageView!
+    @IBOutlet weak var lblname: UILabel!
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        bgview.layer.cornerRadius = 10
+        bgview.backgroundColor = UIColor.lightGray
+        imgcontact.layer.cornerRadius = 30
     }
 }
 
